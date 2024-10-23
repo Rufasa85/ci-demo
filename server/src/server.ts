ef52aec65748fe7bebc40e5eee5cc813e9d7988d
Joe Rehfuss <jrehfuss@2u.com>
2024-10-23 15:41:45 -0700

    updates

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,23 +1,22 @@
-import express from "express";
+import express from 'express';
 
-const PORT = process.env.PORT || 3001;
-
-import db from "./config/connection.js";
-import routes from "./routes/index.js";
+import db from './config/connection.js';
+import routes from './routes/index.js';
 
 await db();
 
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../client/dist"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('../client/dist'));
 
-  app.get("*", (_req, res) => {
-    res.sendFile("../client/dist/index.html");
+  app.get('*', (_req, res) => {
+    res.sendFile('../client/dist/index.html');
   });
 }
 
