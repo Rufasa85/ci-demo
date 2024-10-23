8d9617244c212027f651e9eed56def8409984bfc
Joe Rehfuss <jrehfuss@2u.com>
2024-10-23 16:21:14 -0700

    updates

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     open: true,
     proxy: {
-      '/api': {
-        target: 'https://test-deploy-ci.onrender.com',
+      "/api": {
+        target: "https://test-deploy-ci.onrender.com",
         secure: false,
         changeOrigin: true,
       },
